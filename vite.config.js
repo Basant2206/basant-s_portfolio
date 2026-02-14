2268a3f2925999b625dc6d197c45597fc50046ea
basant <basantprajapati2013@gmail.com>
2026-02-14 21:41:00 +0530

    2 nd update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/basant-s_portfolio/",
   server: {
     port: 3000,
-    host: true, // Listen on all addresses (0.0.0.0)
+    host: true,
   }
 })
+
